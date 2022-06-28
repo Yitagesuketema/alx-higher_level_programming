b5ff781053976fd2eeefa850d3ea0b093bf2665a
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-06-28 23:07:18 +0300

    lastdigit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = int(repr(number[-1]));
+print(f"Last digit of {number} is {last_digit}")
+if last_digit > 5:
+    print(f"{last_digit} is greater than 5", end='')
+elif last_digit == 0:
+    print(f"{last_digit} is greater than 0", end='')
+else:
+    print(f"{last_digit} is less than 6 and not 0")
+    
