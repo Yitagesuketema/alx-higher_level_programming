6c1ea6bc838f3e667f9cba2e4e5151cbe99dfd4a
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 15:40:00 +0300

    replace element in a list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+# Replace element
+# Auth - Yitagesu K Areda
+
+def replace_in_list(my_list, idx, element):
+    while idx >= 0 and idx <= (len(my_list))-1:
+        my_list[idx]=element
+        return (my_list)
+    return (my_list)
