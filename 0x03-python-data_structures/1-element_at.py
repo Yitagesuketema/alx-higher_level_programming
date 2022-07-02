9884c4351f7b83630075daddc254059006a28534
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 14:45:10 +0300

    retrieve an alement from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # 1-element_at.py
+# Secure access to an element in a list
 # Auth - Yitagesu K Areda
 
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return (None)
     else:
-        return (my_list[idx])
+        return (my_list[idx]
