30715eb0211d1444fe6ab23e408d601e5cc7d5bf
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 16:23:55 +0300

    print string in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-print_reversed_list_integer.py
+# Print a list of integers... in reverse!
+# Auth - Yitagesu K Areda
+
+def print_reversed_list_integer(my_list=[]):
+    for x in range (1,len(my_list)+1) :
+        print("{}".format(my_list[-x]))
