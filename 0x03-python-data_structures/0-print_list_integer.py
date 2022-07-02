5029ce7f3b1ff94687b1cacc9f90823b8c086817
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 14:23:55 +0300

    printing all int final

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """Print all integers of a list."""
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+# 0-print_list_integer.py
+# Auth - Yitagesu K Areda
+# Print all integers of a list
+def main():
+    def print_list_integer(my_list=[]):
+        for x in range(len(my_list)):
+            print("{}".format(my_list[x]))
+if __name__== "__main__":
+    main()
