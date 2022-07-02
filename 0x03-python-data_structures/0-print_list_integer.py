89bb9384b6649423cc9d1274cceaff32f77c751f
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 14:27:45 +0300

    extra whitespace removed

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,7 @@
 # 0-print_list_integer.py
 # Auth - Yitagesu K Areda
 # Print all integers of a list
-if __name__== "__main__":
-    def print_list_integer(my_list=[]):
-        for x in range(len(my_list)):
-            print("{:d}".format(my_list[x]))
+
+def print_list_integer(my_list=[]):
+    for x in range(len(my_list)):
+        print("{:d}".format(my_list[x]))
