e9a7ac016cf01e9147f519a5fa939a0629176d65
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 14:25:30 +0300

    main function removed

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,9 +2,7 @@
 # 0-print_list_integer.py
 # Auth - Yitagesu K Areda
 # Print all integers of a list
-def main():
+if __name__== "__main__":
     def print_list_integer(my_list=[]):
         for x in range(len(my_list)):
             print("{}".format(my_list[x]))
-if __name__== "__main__":
-    main()
