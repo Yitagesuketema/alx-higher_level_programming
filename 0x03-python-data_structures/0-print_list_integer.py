b45f9162c9ca256c991aaf783f4407cbe2b61b56
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 13:41:11 +0300

    printing list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 # 0-print_list_integer.py
 # Auth - Yitagesu Ketema
-
-def main ():
-    def print_list_integer(my_list=[]):
-        for x in range(len(my_list)):
-            print("{:d}".format(my_list[x]))
-if __name__ == "__main__":
-    main()
+def print_list_integer(my_list=[]):
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
