b092197582afcb36c7fb074ab9dc0fcef682715d
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 15:54:38 +0300

    white space removed

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@
 # Auth - Yitagesu K Areda
 
 def replace_in_list(my_list, idx, element):
-    while idx >= 0 and idx < (len(my_list))-1:
+    while idx >= 0 and idx < len(my_list):
         my_list[idx] = element
         return (my_list)
     return (my_list)
