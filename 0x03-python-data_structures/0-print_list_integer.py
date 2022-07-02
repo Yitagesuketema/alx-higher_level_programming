ecc11af738e80aa315ba78a4732c9f95155dc72d
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 13:02:50 +0300

    Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/bin/python3
-
-def print_list_integer(my_list=[]):
-    for x in range(0,len(my_list)):
-        text="{number:d}"
-        print(text.format(number=my_list[x]))
+if __name__=="__main__":
+    def print_list_integer(my_list=[]):
+        for x in range(0,len(my_list)):
+            text="{number:d}"
+            print(text.format(number=my_list[x]))
