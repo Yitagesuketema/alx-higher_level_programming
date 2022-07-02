f184c704b159440354732d20e0effbd0c1826892
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 17:47:56 +0300

    replace in a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File - 4-new_in_list.py
+# Replace in a copy
+# Auth - Yitagesu K Areda
+
+
+def new_in_list(my_list, idx, element):
+    while idx >=0 and idx < len(my_list):
+        copy_list = my_list.copy()
+        copy_list[idx] = element
+        return(copy_list)
+    return (my_list)
