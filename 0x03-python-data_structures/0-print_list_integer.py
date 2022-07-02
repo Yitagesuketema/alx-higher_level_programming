56fa3ed1e9d32fc8b41921d8732adf91dcfc0485
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-02 13:38:10 +0300

    printing list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,10 @@
-#!/bin/python3
-def main():
+#!/usr/bin/python3
+# 0-print_list_integer.py
+# Auth - Yitagesu Ketema
+
+def main ():
     def print_list_integer(my_list=[]):
         for x in range(len(my_list)):
             print("{:d}".format(my_list[x]))
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
