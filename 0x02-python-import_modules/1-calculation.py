9aaac2f827667a32051a9eb0588430cf65e1a190
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-03 19:09:27 +0300

    over indentation fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
-    from calculator_1 import add, sub, mul, div
-
+    from calculation_1 import add, sub, mul,div
     a = 10
     b = 5
-
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print(add(a + b))
+    print(sub(a - b))
+    print(mul(a * b))
+    print(div(a / b))
