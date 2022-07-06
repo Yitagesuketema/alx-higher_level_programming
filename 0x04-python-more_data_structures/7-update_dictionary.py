860f4aa499093846b1c00f7c44bd480972d3cc80
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 10:30:37 +0300

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File : 7-update_dictionary.py
+# Update dictionary
+# Auth : Yitagesu K Areda
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
+    
