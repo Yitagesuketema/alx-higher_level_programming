d7b30597627163a021e680368c9342e6382b72a6
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 10:32:20 +0300

    simple delete by key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File : 8-simple_delete.py
+# Simple delete by key
+#Auth : Yitagesu K Areda
+
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary)
+    
