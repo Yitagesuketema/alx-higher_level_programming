196b6fa4ff0fe239cceb22bb45bec0dadff9aabc
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 10:23:29 +0300

    only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# File : 4-only_diff_elements.py
+# Only Differents
+# Auth : Yitagesu K Areda
+
+
+def only_diff_elements(set_1, set_2):
+    """Return a set of all elements present in only one set."""
+    return (set_1 ^ set_2)
