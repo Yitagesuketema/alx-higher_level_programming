ed7d8da14b453ce384165899d66966347526d245
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 10:21:14 +0300

    Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File : 3-common_elements.py
+# Present in both
+# Auth -Yitagesu Ketema
+
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
+    
