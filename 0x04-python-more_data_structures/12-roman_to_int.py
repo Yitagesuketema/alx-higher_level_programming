1fa3ea8b115120472e7603e6250e323b26e285ac
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 16:15:10 +0300

    chmod error fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,6 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
-
     roman_dict = {
             "I": 1,
             "V": 5,
@@ -25,9 +24,8 @@ def roman_to_int(roman_string):
             return (0)
 
         if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
+            roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+            num += roman_dict[roman_string[i]] * -1
         else:
             num += roman_dict[roman_string[i]]
     return (num)
