896b6ebacf2521176f4e52b77afd4b06e9e451af
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 13:35:36 +0300

    Token EOF error fixed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -6,4 +6,4 @@
 
 def multiply_by_2(a_dictionary):
     """Return a new dictionary with all values multipled by 2."""
-    return ({k: a_dictionary[k] * 2 for k in a_dictionary}
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
