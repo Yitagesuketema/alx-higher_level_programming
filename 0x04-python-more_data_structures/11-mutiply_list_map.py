ed13b469f0afca888363ac695e2befa03f9d9f9f
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 10:37:48 +0300

    Multiply by using map

---
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# File : 11-mutiply_list_map.py
+# Multiply by using map
+
+
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x*number, my_list)))
+    
