5c8b6addf6cb41c88b2a61ad0dd54e9145c499be
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 10:26:34 +0300

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File : 5-number_keys.py
+# Number of keys
+# Auth : Yitagesu K Areda
+
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
+    
