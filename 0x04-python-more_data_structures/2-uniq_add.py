bd4f3f5911f538cd49235b6e6026748572c3bb1f
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 10:20:27 +0300

    Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File : 2-uniq_add.py
+# Unique addition
+# Auth : Yitagesu K Areda
+
+
+def uniq_add(my_list=[]):
+    result = 0
+    for x in set(my_list):
+        result += x
+    return (result)
+    
