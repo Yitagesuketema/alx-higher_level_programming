3be9bcce3c4434644d01c90a3677d129d7241c7c
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-06 10:19:31 +0300

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# File : 1-search_replace.py
+# Search and replace
+# Auth : Yitagesu K Areda
+
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
+    
