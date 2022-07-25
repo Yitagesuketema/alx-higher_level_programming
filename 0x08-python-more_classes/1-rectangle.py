aea20a465baab299d19fdd18f189d50950c2830b
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-07-25 17:43:59 +0300

    logical operator updated

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 0-rectangle.py
+# 1-rectangle.py
 # Yitagesu K Areda
 """Defines a Rectangle class."""
 
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,6 +48,6 @@ class Rectangle:
     def perimeter(self):
         """ Return the perimeter of a Rectangle """
 
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
