a872bea93b0109ddf30a1f94f19190029831d005
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-08-02 15:42:42 +0300

    character length updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # 9-add_item.py
-# Yitagesu K Areda"""Add all arguments to a Python list and save them to a file."""
+# Yitagesu K Areda
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
