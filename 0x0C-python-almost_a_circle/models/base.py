c5eb5e2a161ad599ae26391b6c861af1f12bbf6b
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-08-07 21:23:49 +0300

    Pycodestyle error E225 fiexd

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Define a class base """
+
+
 class base:
-
-
     __nb_objects = 0
     """define constructor function"""
+
     def __init__(self, id=None):
         if (id is not None):
             id = self.__nb_objects
         else:
-            self.__nb_objects +=1
+            self.__nb_objects += 1
