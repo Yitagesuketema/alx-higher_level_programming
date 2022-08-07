7da820ff4f5862f6e415c188050b6d80ef46807d
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-08-07 21:19:16 +0300

    pycodestyle error E302 and E711 fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Define a class base """
 class base:
+
+
     __nb_objects = 0
     """define constructor function"""
     def __init__(self, id=None):
-        if (id != None):
+        if (id is not None):
             id = self.__nb_objects
         else:
             self.__nb_objects +=1
