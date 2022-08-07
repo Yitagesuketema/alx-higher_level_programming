8078c15f8503d04cc6337663eccae367b06cdea7
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-08-07 21:14:10 +0300

    base class created

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Define a class base """
+class base:
+    __nb_objects = 0
+    """define constructor function"""
+    def __init__(self, id=None):
+        if (id != None):
+            id = self.__nb_objects
+        else:
+            self.__nb_objects +=1
