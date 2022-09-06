f4ce9b2e7b1d530a9cbcfa4ee73f45788ca37319
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-06 20:00:28 +0300

    - removed from comment

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
----insert id=89 and name =Best School in to first_table table.
+-- insert id=89 and name =Best School in to first_table table.
 INSERT INTO first_table(id, name) VALUES (89, "Best School");
