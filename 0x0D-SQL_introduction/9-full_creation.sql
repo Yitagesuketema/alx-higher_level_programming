38626a51df1e1b223572923582ca28c5422c1454
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-06 20:03:08 +0300

    - removed from first line

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---- multiple query execution.
+-- multiple query execution.
 CREATE TABLE IF NOT EXISTS second_table (id INT , name VARCHAR(256), score INT);
 INSERT IGNORE INTO  second_table (id, `name`, score) VALUES (1, "Jhon", 10);
 INSERT IGNORE INTO  second_table(id, `name`, score) VALUES (2, "Alex", 3);
