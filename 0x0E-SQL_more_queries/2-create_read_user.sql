1247b91ac58823983ea01ad0a5f166c8e1141cfb
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-07 20:55:33 +0300

    decriptive comment added

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,11 @@
--- create the database hbtn_0d_2 and the user user_0d_2
--- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
--- The user_0d_2 password should be set to user_0d_2_pwd
--- If the database hbtn_0d_2 already exists, your script should not fail
--- If the user user_0d_2 already exists, your script should not fail
-CREATE USER  IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+-- create the database hbtn_0d_2 if not exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- create user user_0d_2 @ localhost with pasword user_0d_1_pwd.
+CREATE USER  IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+---grant SELECT privileges to user user_0d_2 at local host.
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+---flush privileges
 FLUSH PRIVILEGES;
