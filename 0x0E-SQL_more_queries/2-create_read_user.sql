05d3404ba542af7ea8090b952100717e0cc7baaf
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-07 20:43:33 +0300

    identified clause removed from grant

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- creating the database hbtn_0d_2 and the user user_0d_2.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+CREATE USER  IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
