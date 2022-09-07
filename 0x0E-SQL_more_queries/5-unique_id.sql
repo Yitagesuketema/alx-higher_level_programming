0f845fc5f390b432c6cf0ecd70d6b2a8c398be9b
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-07 21:47:05 +0300

    table unique_id with unique id and default value 1

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,4 @@
+-- creates the table unique_id in MySQL server if not exists.
+-- id INT with the default value 1 and must be unique.
+-- name VARCHAR(256).
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
