0f08c693a36e709ea924e6b8991d3b749eeb34e4
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-07 21:10:05 +0300

    grant select prevlg

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,6 +6,6 @@ CREATE USER  IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant SELECT privileges to user user_0d_2 at local host.
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 -- flush privileges
 FLUSH PRIVILEGES;
