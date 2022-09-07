98cba5f46e199cf2400475581237fc5894c59374
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-07 22:03:03 +0300

    satates table with unique,auto generated,primary key id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,15 @@
+-- creates the database hbtn_0d_usa if not exists.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- creates the table 'states' in the database 'hbtn_0d_usa' if not exists,
+-- id INT unique, auto generated, can’t be null and is a primary key,
+-- name VARCHAR(256) can’t be null.
+
+CREATE TABLE states (
+
+ id INT UNIQUE AUTO_INCREMENT,
+ name VARCHAR(256) NOT NULL,
+ PRIMARY KEY (id)
+
+ );
