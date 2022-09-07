291bfce1df676aa54c5ea25bea5e2e3582ff57d4
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-07 20:27:19 +0300

    single quote removed

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- creating mysql user with root access.
-CREATE USERIF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON *.*  TO 'user_0d_1'@'localhost'  IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON *.*  TO user_0d_1@localhost  IDENTIFIED BY user_0d_1_pwd;
 FLUSH PRIVILEGES;
