ffec52b0c37736c321cbb1750790a458a314a3f1
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-10-03 22:28:05 +0300

    increment logic updated

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,11 @@ let myObject = {
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
-  this.value++;
+  this.value += 1;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
