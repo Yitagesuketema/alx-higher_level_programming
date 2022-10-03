3f345f676ea2cdef665aa8649478fd26a039ee0e
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-10-03 21:49:09 +0300

    command line arg

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const arglen = process.argv.length;
+if (arglen === 2)
+console.log('No Argument');
+
+else if (arglen === 3)
+console.log('Argument found'); else console.log('Arguments found');
