7246d946bbbe01b43190dcadadf9fe80c61ba8cf
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-10-03 22:29:57 +0300

    extra space removed

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-let myObject = {
+const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-
 myObject.incr = function () {
-  this.value += 1;
+  myObject.value += 1;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
